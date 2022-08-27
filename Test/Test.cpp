4526b532af1c2eb7cee5dcaa1f8b2e2faf7f6a76
Pierre-Luc Gagné <pluckgagne@hotmail.com>
2022-08-26 23:44:25 -0400

    Add get_all_corners(Piece)

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -4,6 +4,9 @@
 #include <cassert>
 #include <vector>
 
+#include "magic_enum.hpp"
+#include "range/v3/all.hpp"
+
 
 //Blokus Game Board(400 squares)
 //84 game pieces(four 21 - piece sets of red, green, blue, and yellow)
@@ -34,14 +37,19 @@
 
 // Let compute the piece_corner of a specific piece
 // First we need a test case, start with the single square
-// In this case, the piece_corner will be {0,0,NW}, {0,0,NE}, {0,0,SW}, and {0,0,SE}
+// In this case, the piece_corners will be {0,0,NW}, {0,0,NE}, {0,0,SW}, and {0,0,SE}
+// The generalization would be that the piece_corners are the unique corner from all the piece squares' corners
+// For this we need to find the equivalent corner of 2 adjacent squares
 
 class Position {
 public:
-    Position(int x, int y)
+    constexpr Position(int x, int y)
         : x(x)
         , y(y)
     {}
+
+    int get_x() const { return x; }
+    int get_y() const { return y; }
 
 private:
     int x;
@@ -50,32 +58,161 @@ private:
     friend auto operator<=>(const Position&, const Position&) = default;
 };
 
+class PositionDelta {
+public:
+    constexpr PositionDelta(int x, int y)
+        : x(x)
+        , y(y)
+    {}
+
+    int get_x() const { return x; }
+    int get_y() const { return y; }
+
+private:
+    int x;
+    int y;
+
+    friend auto operator<=>(const PositionDelta&, const PositionDelta&) = default;
+};
+
+constexpr Position operator+(const Position& position, const PositionDelta& delta) {
+    return { position.get_x() + delta.get_x(), position.get_y() + delta.get_y() };
+}
+
 enum class CornerId { NW, NE, SE, SW };
 
 class Corner {
 public:
-    Corner( Position position, CornerId cornerId)
+    constexpr Corner( Position position, CornerId cornerId)
         : position(std::move(position))
-        , cornerId(cornerId)
+        , corner_id(cornerId)
     {}
+
+    const Position& get_position() const { return position; }
+    CornerId get_corner_id() const { return corner_id; }
 
 private:
     Position position;
-    CornerId cornerId;
+    CornerId corner_id;
 
     friend auto operator<=>(const Corner&, const Corner&) = default;
 };
 
+class Piece {
+public:
+    Piece(std::vector<Position> squares)
+        : squares(std::move(squares))
+    {}
 
-std::vector< Corner > compute_piece_corner() {
-    return { {{0, 0}, CornerId::NW}, {{0, 0}, CornerId::NE}, {{0, 0}, CornerId::SW}, {{0, 0}, CornerId::SE} };
+    const std::vector<Position>& get_squares() const { return squares; }
+private:
+    std::vector<Position> squares;
+};
+
+std::vector<Corner> create_corners(const Position& position) {
+    return { {position, CornerId::NW}, {position, CornerId::NE}, {position, CornerId::SE}, {position, CornerId::SW} };
 }
 
+auto get_all_corners(const Piece& piece) {
+    const auto& squares = piece.get_squares();
+
+    return ranges::views::join(
+        squares |
+        ranges::views::transform([](const Position& position) { return create_corners(position); })
+    );
+}
+
+bool are_equivalent(const Corner& lhs, const Corner& rhs) {
+    // Same position, validate corner_id
+    if (lhs.get_position() == rhs.get_position()) {
+        return lhs.get_corner_id() == rhs.get_corner_id();
+    }
+
+    constexpr PositionDelta left( -1,  0);
+    constexpr PositionDelta up(    0,  1);
+    constexpr PositionDelta right( 1,  0);
+    constexpr PositionDelta down(  0, -1);
+
+    static_assert(magic_enum::enum_count<CornerId>() == 4, "New case needs to be added here");
+    switch (lhs.get_corner_id())
+    {
+    case CornerId::NW:
+        return
+            rhs.get_corner_id() == CornerId::NE && lhs == Corner{ rhs.get_position() + right, CornerId::NW } ||
+            rhs.get_corner_id() == CornerId::SW && lhs == Corner{ rhs.get_position() + down,  CornerId::NW };
+    case CornerId::NE:
+        return
+            rhs.get_corner_id() == CornerId::SE && lhs == Corner{ rhs.get_position() + down,  CornerId::NE } ||
+            rhs.get_corner_id() == CornerId::NW && lhs == Corner{ rhs.get_position() + left,  CornerId::NE };
+    case CornerId::SE:
+        return
+            rhs.get_corner_id() == CornerId::SW && lhs == Corner{ rhs.get_position() + left,  CornerId::SE } ||
+            rhs.get_corner_id() == CornerId::NE && lhs == Corner{ rhs.get_position() + up,    CornerId::SE };
+    case CornerId::SW:
+        return
+            rhs.get_corner_id() == CornerId::NW && lhs == Corner{ rhs.get_position() + up,    CornerId::SW } ||
+            rhs.get_corner_id() == CornerId::SE && lhs == Corner{ rhs.get_position() + right, CornerId::SW };
+    default:
+        assert(false && "Invalid CornerId");
+        return false;
+    }
+}
+
+template<class Rng>
+bool is_unique(const Corner& cornerToValidate, Rng&& corners) {
+    return ranges::none_of(std::forward<Rng>(corners), [&cornerToValidate](const Corner& corner) {
+        const bool isSame = corner == cornerToValidate;
+        return !isSame && are_equivalent(corner, cornerToValidate);
+    });
+}
+
+template<class Rng>
+auto get_unique_corners(Rng&& corners) {
+    return corners | ranges::views::filter([&corners](const Corner& corner) { return is_unique(corner, corners); });
+}
+
+std::vector< Corner > compute_piece_corner(const Piece& piece) {
+    auto allCorners = get_all_corners(piece);
+    auto uniqueCorners = get_unique_corners(allCorners);
+
+    return uniqueCorners | ranges::to<std::vector>();
+}
+
+// ----------------------------------------------------------------------------
+
+void test_are_equivalent() {
+    assert(are_equivalent({ {0, 0}, CornerId::NW }, { {0, 0}, CornerId::NW }));
+    
+    assert(!are_equivalent({ {0, 0}, CornerId::NW }, { {0, 0}, CornerId::NE }));
+
+    assert(are_equivalent({ {0, 0}, CornerId::NW }, { {-1,  0}, CornerId::NE }));
+    assert(are_equivalent({ {0, 0}, CornerId::NW }, { { 0,  1}, CornerId::SW }));
+    assert(are_equivalent({ {0, 0}, CornerId::NE }, { { 0,  1}, CornerId::SE }));
+    assert(are_equivalent({ {0, 0}, CornerId::NE }, { { 1,  0}, CornerId::NW }));
+    assert(are_equivalent({ {0, 0}, CornerId::SE }, { { 1,  0}, CornerId::SW }));
+    assert(are_equivalent({ {0, 0}, CornerId::SE }, { { 0, -1}, CornerId::NE }));
+    assert(are_equivalent({ {0, 0}, CornerId::SW }, { { 0, -1}, CornerId::NW }));
+    assert(are_equivalent({ {0, 0}, CornerId::SW }, { {-1,  0}, CornerId::SE }));
+
+    assert(!are_equivalent({ {0, 0}, CornerId::NW }, { {1, 0}, CornerId::SE }));
+}
+
+void test_is_unique() {
+    assert(is_unique({ {0, 0}, CornerId::NW }, std::vector<Corner>{}));
+    assert(is_unique({ {0, 0}, CornerId::NW }, std::vector<Corner>{ { {0, 0}, CornerId::NE } }));
+
+    assert(!is_unique({ {0, 0}, CornerId::NW }, std::vector<Corner>{ { {0, 0}, CornerId::NW } }));
+    assert(!is_unique({ {0, 0}, CornerId::NW }, std::vector<Corner>{ { {0, 1}, CornerId::SW } }));
+}
 
 void test_get_piece_corner() {
-    assert(compute_piece_corner() == std::vector< Corner >({ {{0, 0}, CornerId::NW}, {{0, 0}, CornerId::NE}, {{0, 0}, CornerId::SW}, {{0, 0}, CornerId::SE} }));
+    using namespace ranges::actions;
+
+    assert(sort(compute_piece_corner({ { {0,0} } })) == sort(std::vector< Corner >({ {{0, 0}, CornerId::NW}, {{0, 0}, CornerId::NE}, {{0, 0}, CornerId::SE}, {{0, 0}, CornerId::SW} })));
+    assert(sort(compute_piece_corner({ { {0,0}, {1,0} } })) == sort(std::vector< Corner >({ {{0, 0}, CornerId::NW}, {{1, 0}, CornerId::NE}, {{1, 0}, CornerId::SE}, {{0, 0}, CornerId::SW} })));
 }
 
 int main() {
+    test_are_equivalent();
     test_get_piece_corner();
 }
